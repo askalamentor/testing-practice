baabfbe309fb3e32fc99cadf8ce37b0854e4e808
Ugur Yilmaz <ugurbiyomek@gmail.com>
2022-12-23 20:06:50 +0300

    Add capitalize.js

--- functions/capitalize.js
+++ functions/capitalize.js
@@ -1,1 +1,16 @@
+function capitalize(string) {
+  for (let i = 0; i < string.length; i++) {
+    if (string.charCodeAt(i) >= 97 && string.charCodeAt(i) <= 122) {
+      return (
+        string.slice(0, i) +
+        string[i].toUpperCase() +
+        string.slice(i + 1, string.length)
+      );
+    } else if (string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90) {
+      return string;
+    }
+  }
+  return 'no valid characters';
+}
 
+export default capitalize;
