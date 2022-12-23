c453c08844b5fb9a3e34a02068755e5a568c9754
Ugur Yilmaz <ugurbiyomek@gmail.com>
2022-12-23 21:45:00 +0300

    Add calculator.js

--- functions/calculator.js
+++ functions/calculator.js
@@ -1,1 +1,13 @@
+export default class Calculator {
+  add(x, y) {
+    if (typeof x === 'string') x = parseFloat(x);
+    if (typeof y === 'string') y = parseFloat(y);
+    return Number((x + y).toFixed(2));
+  }
 
+  substract(x, y) {
+    if (typeof x === 'string') x = parseFloat(x);
+    if (typeof y === 'string') y = parseFloat(y);
+    return Number((x - y).toFixed(2));
+  }
+}
