e05d6d6af5895339c4f30ac91e07b09a4f6eb4e9
Ugur Yilmaz <ugurbiyomek@gmail.com>
2022-12-23 20:12:42 +0300

    Update comment in capitalize.js

--- functions/capitalize.js
+++ functions/capitalize.js
@@ -1,15 +1,18 @@
 function capitalize(string) {
   for (let i = 0; i < string.length; i++) {
+    // if char is lowercase
     if (string.charCodeAt(i) >= 97 && string.charCodeAt(i) <= 122) {
       return (
         string.slice(0, i) +
         string[i].toUpperCase() +
         string.slice(i + 1, string.length)
       );
+      // if char is uppercase
     } else if (string.charCodeAt(i) >= 65 && string.charCodeAt(i) <= 90) {
       return string;
     }
   }
+  // if there is no letter in string
   return 'no valid characters';
 }
 
