867ab0ee729b9eb2daa39adcb91c0b7215b89d70
Ugur Yilmaz <ugurbiyomek@gmail.com>
2022-12-25 16:12:32 +0300

    Update caesarCipher function and test

--- functions/caesarCipher.js
+++ functions/caesarCipher.js
@@ -1,14 +1,34 @@
-export default function caesarCipher(string) {
-  // check any input
-  if (!string) return 'no input';
-  // check true input type
-  if (typeof string !== 'string') return 'no string';
+export default function caesarCipher(text, shift) {
+  // check: text exists, text input type is text
+  if (!text) return 'no text available';
+  if (typeof text !== 'string') return 'text is not string';
+  // check: shift exitst, shift input type is number
+  if (!shift) return 'no shift available';
+  if (typeof shift !== 'number') return 'shift is not number';
 
-  const letters = string.split('');
+  const letters = text.split('');
   const cipher = [];
 
   letters.forEach((letter) => {
-    cipher.push(String.fromCharCode(letter.charCodeAt(0) + 1));
+    // tolowercase
+    let asciiVal = letter.toLowerCase();
+
+    // if it is blank, stays blank
+    if (asciiVal.charCodeAt(0) === 32) {
+      asciiVal;
+    }
+    // otherwise change letter
+    else {
+      asciiVal = String.fromCharCode(letter.charCodeAt(0) + shift);
+    }
+
+    // check end of lowercase ascii (z = 122)
+    if (asciiVal.charCodeAt(0) > 122) {
+      // (asciivalue) - (z ascii (122)) + (a ascii (97)) - 1
+      asciiVal = String.fromCharCode(asciiVal.charCodeAt(0) - 122 - 1 + 97);
+    }
+
+    cipher.push(asciiVal);
   });
 
   return cipher.join('');
--- tests/caesarCipher.test.js
+++ tests/caesarCipher.test.js
@@ -1,5 +1,17 @@
 import caesarCipher from '../functions/caesarCipher';
 
-test('"aaabbbcccdddeee" should become "bbbcccdddeeefff"', () => {
-  expect(caesarCipher('aaabbbcccdddeee')).toBe('bbbcccdddeeefff');
+test('"aaabbbcccdddeee", 1 should become "bbbcccdddeeefff"', () => {
+  expect(caesarCipher('aaabbbcccdddeee', 1)).toBe('bbbcccdddeeefff');
+});
+
+test('"adz", 3 should become "dgc"', () => {
+  expect(caesarCipher('adz', 3)).toBe('dgc');
+});
+
+test('"abcd cdef xyz", 2 should become "cdef efgh zab"', () => {
+  expect(caesarCipher('abcd cdef xyz', 2)).toBe('cdef efgh zab');
+});
+
+test('1, 1 should become "text is not string"', () => {
+  expect(caesarCipher(1, 1)).toBe('text is not string');
 });
